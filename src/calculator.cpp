0faf5bb9e96b6c21a96d6296785861f2140ee183
cyberbebebe <misha2005serg@gmail.com>
2024-12-19 00:25:50 +0200

    Add files

---
+++ src/calculator.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Mac-10 on 19.12.2024.
+//
+
+#include "calculator.h"
+
+int Add(const int a, const int b)
+{
+    return a + b;
+}
+
+int Sub(const int a, const int b)
+{
+    return Add(a, -b);
+}
---
+++ src/calculator.h
@@ -0,0 +1,13 @@
+//
+// Created by Mac-10 on 19.12.2024.
+//
+
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+
+int Add(int, int);
+int Sub(int, int);
+
+
+#endif//CALCULATOR_H
